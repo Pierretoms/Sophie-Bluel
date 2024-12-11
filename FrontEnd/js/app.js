5819815366bc62a0a79e8f0aab8d18bb0933a263
Pierre toms <pierre.toms13@gmail.com>
2024-12-11 12:56:33 +0100

    ajout fonction categories et filtres + mise en place des filtres css de l'étape 1.2

--- FrontEnd/js/app.js
+++ FrontEnd/js/app.js
@@ -8,7 +8,6 @@ async function getWorks() {
         
         const json = await response.json();
         console.log(json);
-        // setFigure(json);
         for(let i = 0; i < json.length; i++) {
             setFigure(json[i])
         }
@@ -19,9 +18,36 @@ async function getWorks() {
 getWorks();
 
 function setFigure(data) {
-const figure = document.createElement("figure")
-figure.innerHTML = `<img src=${data.imageUrl} alt=${data.title}>
+    const figure = document.createElement("figure")
+    figure.innerHTML = `<img src=${data.imageUrl} alt=${data.title}>
 				<figcaption>${data.title}</figcaption>`;
+    
+    document.querySelector(".gallery").append (figure);
+}
 
-document.querySelector(".gallery").append (figure);
+// http://localhost:5678/api/categories
+
+async function getCategories() {
+    const url = "http://localhost:5678/api/categories";
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        
+        const json = await response.json();
+        console.log(json);
+        for(let i = 0; i < json.length; i++) {
+            setFiltres(json[i])
+        }
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+getCategories();
+
+function setFiltres(data) { 
+    const div = document.createElement("div");
+    div.innerHTML = `${data.name}`;
+    document.querySelector(".div-filtres").append (div);
 }
