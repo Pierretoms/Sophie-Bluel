6e99d7bbd6e283b6d68b6f64526702809462687a
Pierre toms <pierre.toms13@gmail.com>
2024-12-25 23:02:45 +0100

    ajout de la modal test

--- FrontEnd/js/app.js
+++ FrontEnd/js/app.js
@@ -61,11 +61,76 @@ init();
 function modeEdition() {
     if (sessionStorage.authToken) {
         console.log("ok");
-        const banniereEdit = document.createElement("div")
-        banniereEdit.className = 'mode-edit'
-        banniereEdit.innerHTML = '<p><i class="fa-regular fa-pen-to-square"></i>Mode édition</p>'
-        document.body.prepend(banniereEdit)
+        const banniereEdit = document.createElement("div");
+        banniereEdit.className = 'mode-edit';
+        banniereEdit.innerHTML = '<p><i class="fa-regular fa-pen-to-square"></i>Mode édition</p>';
+        document.body.prepend(banniereEdit);
     } 
 }
 
 modeEdition()
+
+let modal = null;
+const focusableSelector = 'button, a, input, textarea';
+let focusables = []
+
+const openModal = function (e) {
+    e.preventDefault();
+    modal = document.querySelector(e.target.getAttribute('href'));
+    focusables = Array.from(modal.querySelectorAll(focusableSelector));
+    focusables[0].focus();
+    modal.style.display = null;
+    modal.removeAttribute('aria-hidden');
+    modal.setAttribute('aria-modal', 'true');
+    modal.addEventListener('click', closeModal);
+    modal.querySelector('.js-modal-close').addEventListener('click', closeModal);
+    modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation);
+
+};
+
+const closeModal = function (e) {
+    if (modal === null) return
+    e.preventDefault();
+    modal.style.display = "none";
+    modal.setAttribute('aria-hidden', 'true');
+    modal.removeAttribute('aria-modal');
+    modal.addEventListener('click', closeModal);
+    modal.querySelector('.js-modal-close').removeEventListener('click', closeModal);
+    modal.querySelector('.js-modal-stop').removeEventListener('click', stopPropagation);
+    modal = null;
+};
+
+const stopPropagation = function (e) {
+    e.stopPropagation();
+};
+
+const focusInModal = function (e) {
+    e.preventDefault();
+    let index = focusables.findIndex(f => f === modal.querySelector(':focus'));
+    if (e.shiftKey === true) {
+        index--;
+    } else {
+        index++;
+    }
+    index++;
+    if (index >= focusables.length) {
+        index = 0;
+    }
+    if (index < 0) {
+        index = focusables.length - 1;
+    }
+    focusables[index].focus();
+}
+
+window.addEventListener('keydown', function (e){
+    if (e.key === "Escape" || e.key === "Esc") {
+        closeModal(e);
+    }
+    if (e.key === 'Tab' && modal !== null) {
+        focusInModal(e);
+    }
+})
+
+document.querySelectorAll('.js-modal').forEach((a) => {
+    a.addEventListener('click' , openModal);
+});
